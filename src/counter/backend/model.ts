9f55ed5533986b608765eaf2abb81821a4e84bf0
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2016-12-07 15:34:53 +0700

    counter

--- src/counter/backend/model.ts
+++ src/counter/backend/model.ts
@@ -3,4 +3,10 @@ import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Model, Backend } from '../shared';
 export const RxCounters = new ReplaySubject<Model.ICounter[]>(1);
 export const RxServices = new ReplaySubject<Model.Center.IService[]>(1);
+
 export const RxCurrentCounter = new ReplaySubject<Model.ICounter>(1);
+let currentCounter: Model.ICounter = null;
+RxCurrentCounter.subscribe(c => currentCounter = c);
+export function CurrentCounterID() {
+    return currentCounter? currentCounter.id : null;
+}
