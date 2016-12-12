0f9c90ab43d6d8cfbb45db2f9cc2db99fec3027a
Huong Nguyen <Huong Nguyen>
2016-12-12 10:14:46 +0700

    disable recorder

--- src/counter/recorder/index.ts
+++ src/counter/recorder/index.ts
@@ -8,31 +8,35 @@ const config: Config = {
     host: 'http://localhost:3000/room/counter/record?fname=',
 };
 
-var recorder = new MP3Recorder(config);
-recorder.start(function () {
-    console.log('Start recorder!')
-}, function () {
-    alert('We could not make use of your microphone at the moment');
-});
+var isEnable = false;
+if (isEnable) {
+    var recorder = new MP3Recorder(config);
+    recorder.start(function () {
+        console.log('Start recorder!')
+    }, function () {
+        alert('We could not make use of your microphone at the moment');
+    });
 
-const RxRecordFile = Serving.RxData.map(tickets => {
-    if (tickets[0]) {
-        return tickets[0].id + '.mp3'
-    } else {
-        return null
-    }
-})
+    const RxRecordFile = Serving.RxData.map(tickets => {
+        if (tickets[0]) {
+            return tickets[0].id + '.mp3'
+        } else {
+            return null
+        }
+    })
 
-RxRecordFile.subscribe(
-    function (x) {
-        recorder.setSessionID(x)
-    },
-    function (err) {
-        console.log('Error: %s', err);
-    },
-    function () {
-        console.log('Completed');
-    }
-);
+    RxRecordFile.subscribe(
+        function (x) {
+            recorder.setSessionID(x)
+        },
+        function (err) {
+            console.log('Error: %s', err);
+        },
+        function () {
+            console.log('Completed');
+        }
+    );
+
+} 
 
 
