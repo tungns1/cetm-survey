142978d20a48524c7d988d9d21376e2c32a458e3
Bui Nam <Bui Nam>
2016-12-13 08:48:52 +0700

    fix

---
+++ src/report/overview/time.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+
+function TwoDigit(n: number): string {
+    n = Math.round(n);
+    return (n > 9 ? '' : '0') + n;
+}
+
+@Pipe({
+    name: "hour"
+})
+export class HourPipe implements PipeTransform {
+    transform(v: number) {
+        return [v / 3600, (v % 3600) / 60, v % 60].map(TwoDigit).join(":");
+    }
+}
