8efa9d02d38f4cf8efe85f15140a1570dc5559c7
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2016-12-15 10:08:25 +0700

    update build

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+while true; do 
+
+    if [[ $(git pull origin master) == *up-to-date* ]]; 
+    then
+        echo "no change"
+    else
+        echo "detect changes"
+        sleep 2s
+        # npm run build
+    fi
+
+    echo "sleep 30s"
+
+    sleep 30s        
+
+done
