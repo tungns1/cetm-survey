212b2dcc97560401e802d382cc8af3e6afcdf470
Bui Nam <Bui Nam>
2016-12-26 16:59:39 +0700

    fix

--- src/report/sum/sum.pipe.ts
+++ src/report/sum/sum.pipe.ts
@@ -11,7 +11,14 @@ function TwoDigit(n: number): string {
     name: "hour"
 })
 export class HourPipe implements PipeTransform {
-    transform(v: string) {
-        return [+v / 3600, (+v % 3600) / 60, +v % 60].map(TwoDigit).join(":");
+    transform(v: number) {
+        var date = new Date(v * 1000);
+
+        var hours = date.getHours();
+        var minutes = "0" + date.getMinutes();
+        var seconds = "0" + date.getSeconds();
+
+        // Will display time in 10:30:23 format
+        return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
     }
 }
