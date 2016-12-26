db6523ad601b4bd5c5b6c75669e0606f5c563f3d
Bui Nam <Bui Nam>
2016-12-26 13:15:24 +0700

    fix

--- src/counter/queue/missed.component.ts
+++ src/counter/queue/missed.component.ts
@@ -16,8 +16,6 @@ import { combineLatest } from 'rxjs/observable/combineLatest';
     styleUrls: ['waiting.component.css']
 })
 export class MissedComponent {
-    private tk: string;
-    show=true;
     count = Missed.count();
     tickets = combineLatest<Model.House.ITicket[], string>(Missed.RxData, search).map(([tickets, text]) => {
         return tickets.filter(v => v.cnum.indexOf(text) !== -1);
@@ -30,9 +28,5 @@ export class MissedComponent {
         e.preventDefault();
         e.stopPropagation();
         search.next(e.target['value']);
-    }
-    showInput(ticket:string){
-        this.show=!this.show;
-        this.tk='';
     }
 }
