ed388c30690de838238a78555197c2aaa63ad819
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-01-04 14:39:47 +0700

    log

--- src/counter/backend/device/feedback.ts
+++ src/counter/backend/device/feedback.ts
@@ -4,8 +4,14 @@ try {
     var myRequire = window['myRequire'];
     var feedback = myRequire('app/counter/feedback');
     console.log("Feedback is supported");
-    feedback.OnShow(() => feedbackAvailable = true);
-    feedback.OnHide(() => feedbackAvailable = false);
+    feedback.OnShow(() => {
+        feedbackAvailable = true;
+        console.log("Feedback shown");
+    });
+    feedback.OnHide(() => {
+        feedbackAvailable = false;
+        console.log("Feedback hide");
+    });
 } catch (e) {
     console.log("Fail to determine feedback supportability", e);
 }
