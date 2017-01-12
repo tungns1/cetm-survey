364f31d83506bc27bb6110b9dfaef61745c73d9c
Huong Nguyen <Huong Nguyen>
2017-01-12 17:07:41 +0700

    debug

--- src/counter/recorder/index.ts
+++ src/counter/recorder/index.ts
@@ -13,7 +13,8 @@ try {
 var isEnable = true;
 if (isEnable) {
     const RxRecordFile = Serving.RxData.map(tickets => {
-        if (tickets[0]) {            
+        if (tickets[0]) {    
+            console.log(tickets[0].branch_id);  
             return [tickets[0].id + '_' + tickets[0].cnum + '.mp3', tickets[0].branch_id];
         } else {
             return [null, null]
