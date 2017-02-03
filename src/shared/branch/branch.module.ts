4a2c8b420dcaff596d2c785b7134477d00ab1222
lehieu1312 <hieulm1312@gmail.com>
2017-02-03 13:02:17 +0700

    fix eng and add es

--- src/shared/branch/branch.module.ts
+++ src/shared/branch/branch.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { BranchSelectorComponent, MultiBranchSelectorComponent } from './branch.component';
 import { BranchPickerComponent } from './branch_picker.component';
 import { BranchNamePipe, LevelNamePipe } from './branch.pipe';
-import { I18n } from '../../shared/';
+import { TranslateModule } from '../i18n';
 
 @NgModule({
     imports: [CommonModule, FormsModule],
-    exports: [BranchSelectorComponent, BranchPickerComponent, MultiBranchSelectorComponent, BranchNamePipe, LevelNamePipe,,I18n.TranslateModule],
+    exports: [BranchSelectorComponent, BranchPickerComponent, MultiBranchSelectorComponent, BranchNamePipe, LevelNamePipe],
     declarations: [BranchSelectorComponent, BranchPickerComponent, MultiBranchSelectorComponent, BranchNamePipe, LevelNamePipe],
 })
 export class BranchModule {
