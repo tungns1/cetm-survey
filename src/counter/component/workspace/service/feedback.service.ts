28ed5d578b72b03a1d014aa15088b16600ea2d10
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-02-16 16:03:08 +0700

    check feedback

--- src/counter/component/workspace/service/feedback.service.ts
+++ src/counter/component/workspace/service/feedback.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { WorkspaceService } from './workspace.service';
-import { ITicket } from '../../shared';
-import { FeedbackDeviceAvailabel$ } from '../../../device';
+import { ITicket, SharedService } from '../../shared';
+import { IsFeedbackDeviceAvailable } from '../../../device';
 import { of } from 'rxjs/observable/of';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
@@ -11,13 +11,23 @@ import { timer } from 'rxjs/observable/timer';
 export class FeedbackService {
 
     constructor(
-        private workspaceService: WorkspaceService
-    ) { }
+        private workspaceService: WorkspaceService,
+        private authService: SharedService.Auth.AuthService
+    ) {
+        this.onInit();
+    }
+
+    private onInit() {
+        this.authService.RxMySetting.subscribe(s => {
+            this.required = s.config.feedback.required;
+        })
+    }
 
     private socket = this.workspaceService.Socket;
     private feedbackDone$ = this.socket.RxEvent<ITicket>("/feedback_done");
     private skipFeedback$ = new ReplaySubject<string>(4, 1000);
     promptForSkip$ = new BehaviorSubject<ITicket>(null);
+    private required = false;
 
     SkipFeedback(t: ITicket) {
         this.promptForSkip$.next(null);
@@ -25,20 +35,19 @@ export class FeedbackService {
     }
 
     CheckFeedback(t: ITicket) {
-        return FeedbackDeviceAvailabel$.switchMap(b => {
-            if (b) {
-                // check if done
-                return this.feedbackDone$.filter(d => d.id === t.id)
-                    .merge(timer(250).first().switchMap(_ => {
-                        // ask to skip
-                        this.promptForSkip$.next(t);
-                        // skip feedback
-                        return this.skipFeedback$.filter(id => id === t.id)
-                    }))
-                    .map(_ => t);
-            }
+        if (!this.required || !IsFeedbackDeviceAvailable()) {
             return of(t);
-        }).first();
+        }
+
+        // check if done
+        return this.feedbackDone$.filter(d => d.id === t.id)
+            .merge(timer(100).first().switchMap(_ => {
+                // ask to skip
+                this.promptForSkip$.next(t);
+                // skip feedback
+                return this.skipFeedback$.filter(id => id === t.id)
+            }))
+            .map(_ => t).first();
     }
 
 }
--- src/counter/device/feedback.ts
+++ src/counter/device/feedback.ts
@@ -1,25 +1,29 @@
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
-export const FeedbackDeviceAvailabel$ = new BehaviorSubject<boolean>(false);
+let feedbackDeviceAvailable = false;
 
 try {
     var myRequire = window['myRequire'];
     var feedback = myRequire('app/counter/feedback');
     console.log("Feedback is supported");
     feedback.OnShow(() => {
-        FeedbackDeviceAvailabel$.next(true);
+        feedbackDeviceAvailable = true;
         console.log("Feedback shown");
     });
     feedback.OnHide(() => {
-        FeedbackDeviceAvailabel$.next(false);
+        feedbackDeviceAvailable = false;
         console.log("Feedback hide");
     });
 } catch (e) {
     console.log("Fail to determine feedback supportability", e);
 }
 
+export function IsFeedbackDeviceAvailable() {
+    return feedbackDeviceAvailable
+}
+
 function test() {
-    FeedbackDeviceAvailabel$.next(true);
+    feedbackDeviceAvailable = true;
 }
 
 // test();
