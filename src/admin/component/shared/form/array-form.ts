3256e871c9d740d2c416e8f60f04c8346a366bc7
lehieu1312 <hieulm1312@gmail.com>
2017-02-20 16:01:49 +0700

    arayform

--- src/admin/component/shared/form/array-form.ts
+++ src/admin/component/shared/form/array-form.ts
@@ -16,7 +16,7 @@ import { FormArray, FormControl } from '@angular/forms';
 @Component({
     selector: 'arr-form',
     template: `
-        <i class="fa fa-plus" (click)="add()" translate=""> BUTTON_ADD </i>        
+        <i class="fa fa-plus" (click)="add()" translate>  </i>        
         <div *ngFor="let c of values; let i = index;" >
             <input class="iput-fnews-hlm hl-input" [ngModel]="values[i]" (change)="onChange($event, i)" /> 
             <i class="fa fa-trash pointer" (click)="remove(i)"> </i>
