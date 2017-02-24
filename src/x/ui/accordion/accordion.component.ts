794a958c1b344feea09c1dc62282bd8cc3348305
Tan Vuong Duc <Tan Vuong Duc>
2017-02-24 11:46:10 +0700

    -add accordion for the filters -can click on text in select_check

---
+++ src/x/ui/accordion/accordion.component.ts
@@ -0,0 +1,38 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'accordion-title',
+	template: `<ng-content></ng-content>`
+})
+export class AccordionTitleComponent {
+
+}
+
+@Component({
+	selector: 'accordion',
+	template: `
+	<div (click)="Toggle()">
+		<ng-content select="accordion-title"></ng-content>
+        <span *ngIf="isHidden" style="float: right;"><i _ngcontent-lat-10="" class="fa fa-angle-double-right"> </i></span>
+        <span *ngIf="!isHidden" style="float: right;"><i _ngcontent-lat-10="" class="fa fa-angle-double-down"> </i></span>
+	</div>
+	<div [class.hidden]="isHidden">
+		<ng-content></ng-content>
+	</div>
+	`
+})
+export class AccordionComponent {
+	
+	isHidden = true;
+
+	@Input() isOpen : boolean;
+	ngOnInit() {
+		if(this.isOpen) {
+			this.isHidden = false;
+		}
+    }
+
+	Toggle() {
+		this.isHidden = !this.isHidden
+	}
+}
