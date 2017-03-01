e196fdf8c23916f6f619d482a7c192b9ed923296
Bui Nam <Bui Nam>
2017-03-01 17:31:15 +0700

    fix

--- src/report/component/customer/service/customer.service.ts
+++ src/report/component/customer/service/customer.service.ts
@@ -21,11 +21,14 @@ export interface IHistory {
     total: number;
 }
 
+export const  paging = new Paging<ITransactionView>();
+
 @Injectable()
 export class CustomerAPI {
     constructor(
         private filterService: ReportFilterService
     ) { }
+   
 
     GetHistory(filter: ReportFilter, skip: number, limit: number, id: string) {
         const query = Object.assign({
@@ -36,9 +39,18 @@ export class CustomerAPI {
 
         return this.api.Get<IHistory>("customer_history", query);
     }
+    ChuyenTrang(page: number,customer:string) {
+        const skip = paging.SkipForPage(page);
+        const limit = paging.Limit;
+        this.GetHistory(this.filterService.Current, skip, limit,customer)
+            .subscribe(v => {
+                paging.SetPage(page);
+                paging.Reset(v.data, v.total);
+            });
+    }
 
     Search(id: string) {
-        let filter=this.filterService.Current;
+        let filter = this.filterService.Current;
         this.api.Get<IHistory>("customer_history", this.makeQuery(filter, id)).subscribe(v => {
             this.RxCustomer.next(v.data);
         });
