879cbd502b87cc5aedd995521cd5c7f9474bf537
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-03-02 22:34:34 +0700

    redirect

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,8 @@ export const routes: Routes = [
     { path: "admin", loadChildren: "./admin/index#AdminModule" },
     { path: "monitor", loadChildren: "./monitor/index" },
     { path: "report", loadChildren: "./report/index" },
-    { path: "counter", loadChildren: "./counter/index"}
+    { path: "counter", loadChildren: "./counter/index" },
+    { path: "**", redirectTo: "report" }
 ];
 
 export const appRouting = RouterModule.forRoot(routes);
