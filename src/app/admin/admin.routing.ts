1b28a5d5415abaa75165a8cee1bfcd826f68618b
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-03-02 21:41:53 +0700

    mv folder

---
+++ src/app/admin/admin.routing.ts
@@ -0,0 +1,44 @@
+import { Routes, RouterModule } from "@angular/router";
+import { SharedService } from "./shared/";
+import {
+  AdminComponent, OrgModule,
+  CenterModule, HouseModule, MetaModule
+} from "./component";
+
+const children: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "org"
+  },
+  {
+    path: "org",
+    loadChildren: () => OrgModule
+  },
+  {
+    path: "meta",
+    loadChildren: () => MetaModule
+  },
+  {
+    path: "house",
+    loadChildren: () => HouseModule
+  },
+  {
+    path: "center",
+    loadChildren: () => CenterModule
+  }
+]
+
+export const routing = RouterModule.forChild([
+  {
+    path: "",
+    canActivate: [SharedService.Auth.AuthGuard],
+    component: AdminComponent,
+    children: children
+  }
+]);
+
+export const components = children.map(c => c.component).filter(c => !!c);
+
+components.push(AdminComponent);
+
---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from "@angular/router";
+import { AdminModule } from "./admin";
+
+export const routes: Routes = [
+    { path: "admin", loadChildren: "./admin/index#AdminModule" },
+    { path: "monitor", loadChildren: "./monitor/index" },
+    { path: "report", loadChildren: "./report/index" },
+    { path: "counter", loadChildren: "./counter/index"},
+    { path: "**", pathMatch: "full", redirectTo: "admin" }
+];
+
+export const appRouting = RouterModule.forRoot(routes);
