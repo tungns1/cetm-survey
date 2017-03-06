42d77013e9b0f72a0091dea16dd60ebcb49fead3
TAN\Tan <tanvd@miraway.vn>
2017-03-06 11:26:59 +0700

    fall back pre-ver of report - history

--- src/app/report/component/history/history.component.ts
+++ src/app/report/component/history/history.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
 import { ITransactionView } from '../../model';
 import { ReportFilterService, ReportFilter } from '../../service/';
 import { TransactionHistoryApi } from './history.service';
@@ -24,23 +23,6 @@ export class HistoryComponent {
         });
     }
 
-    filterHistory () {
-        console.log('im in');
-    }
-
-    // makeForm(u) {
-    //     u = u || <any>{};
-    //     return (new FormBuilder).group({
-    //     id: [u.id],
-    //     password: ['', u.id ? null : Validators.required], // do not require password on update
-    //     username: [u.username, Validators.required],
-    //     fullname: [u.fullname, Validators.required],
-    //     email: [u.email],
-    //     role: [u.role, Validators.required],
-    //     branch_id: [u.branch_id, Validators.required],
-    //     });
-    // }
-
     chuyenTrang(page: number) {
         const skip = this.paging.SkipForPage(page);
         const limit = this.paging.Limit;
@@ -55,15 +37,15 @@ export class HistoryComponent {
         this.transactionHistoryApi.ExportHistory(this.filterService.Current);
     }
 
-    // rows = [
-    //     { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    //     { name: 'Dany', gender: 'Male', company: 'KFC' },
-    //     { name: 'Molly', gender: 'Female', company: 'Burger King' },
-    // ];
-    // columns = [
-    //     { prop: 'name' },
-    //     { name: 'Gender' },
-    //     { name: 'Company' }
-    // ];
+    rows = [
+        { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+        { name: 'Dany', gender: 'Male', company: 'KFC' },
+        { name: 'Molly', gender: 'Female', company: 'Burger King' },
+    ];
+    columns = [
+        { prop: 'name' },
+        { name: 'Gender' },
+        { name: 'Company' }
+    ];
 
 }
