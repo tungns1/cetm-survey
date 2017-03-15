0579811d374634a70a0f0511f032eb4f39f5c772
Bui Nam <Bui Nam>
2017-03-15 15:35:48 +0700

    fix

--- src/app/report/component/customer/service/customer.service.ts
+++ src/app/report/component/customer/service/customer.service.ts
@@ -52,6 +52,7 @@ export class CustomerAPI {
             if (v.data.length > 0) {
                 this.RxCustomer.next(v.data);
             } else {
+                this.RxCustomer.next(null);
                 alert('Dữ liệu khách hàng không có');
             }
 
@@ -61,6 +62,8 @@ export class CustomerAPI {
         return this.apiCustomer.Get<Model.Org.ICustomer>("get_customer_by_code", { code: code }).subscribe(v => {
             if (v != null) {
                 RxInfoCustomer.next(v);
+            }else{
+                RxInfoCustomer.next(null);
             }
         });
     }
