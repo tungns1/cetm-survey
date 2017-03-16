2a901b9a90aaaae027e9d4f18f2de097a935a101
TAN\Tan <tanvd@miraway.vn>
2017-03-16 11:26:00 +0700

    fix UI in report

--- src/app/x/ui/accordion/accordion.component.ts
+++ src/app/x/ui/accordion/accordion.component.ts
@@ -16,7 +16,7 @@ export class AccordionTitleComponent {
         <span *ngIf="isHidden" style="float: right; color: #9a9a9b;"><i _ngcontent-lat-10="" class="fa fa-caret-down fa-lg"> </i></span>
         <span *ngIf="!isHidden" style="float: right; color: #0b8fdb;"><i _ngcontent-lat-10="" class="fa fa-caret-up fa-lg"> </i></span>
 	</div>
-	<div [class.hidden]="isHidden" style="background-color: #fff; border-top: 1px solid #c1c1c1;">
+	<div [class.hidden]="isHidden" style="background-color: #fff; border-top: 1px solid #a7a9ac;">
 		<ng-content></ng-content>
 	</div>
 	`
