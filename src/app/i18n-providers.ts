6d2328d27e12e64bb35dad0d3d18bcba062aa0e0
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-03-19 22:23:25 +0700

    update translation

--- src/app/i18n-providers.ts
+++ src/app/i18n-providers.ts
@@ -3,6 +3,8 @@ import { GetRaw } from './x/backend';
 import { AppStorage } from '../store';
 import { LOCALES } from '../const';
 
+const i18nFormat = "xmb";
+
 function getLocale() {
   // Get the locale id from the global
   const locale = document['locale'] as string;
@@ -18,11 +20,11 @@ export function getTranslationProviders(): Promise<Object[]> {
     return Promise.resolve(noProviders);
   }
   // Ex: 'locale/messages.fr.xlf`
-  const translationFile = `./locale/${locale}/messages_${locale}.xlf`;
+  const translationFile = `./locale/${locale}/messages_${locale}.${i18nFormat}`;
   return getTranslations(translationFile)
     .then((translations) => [
       { provide: TRANSLATIONS, useValue: translations.body },
-      { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+      { provide: TRANSLATIONS_FORMAT, useValue: i18nFormat },
       { provide: LOCALE_ID, useValue: locale }
     ])
     .catch(() => noProviders); // ignore if file not found
