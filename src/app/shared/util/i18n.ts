44fe4dff5f3ff047e95bd9ae445f132a376a470d
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-03-21 17:46:36 +0700

    fix i18n

--- src/app/shared/util/i18n.ts
+++ src/app/shared/util/i18n.ts
@@ -8,5 +8,6 @@ const defaultCulture = LOCALES.DEFAULT;
 const culture = AppStorage.Culture;
 
 export function Localize(l: L10nText) {
+    console.log(culture, l);
     return l[culture] || l[defaultCulture];
 }
