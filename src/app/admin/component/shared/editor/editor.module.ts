1b930d5f9c236cdb48769556d00b9fcda11bf45c
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-04-05 13:57:07 +0700

    add confirm message

--- src/app/admin/component/shared/editor/editor.module.ts
+++ src/app/admin/component/shared/editor/editor.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { SharedModule, TranslateModule } from '../../../shared';
+import { SharedModule, TranslateModule, Ng2BasicModule } from '../../../shared';
 import { EditorComponent, EditorTitleComponent, EditorFieldComponent } from './editor.component';
 
 import { AppDataTableComponent, AppTableFieldComponent } from './table.component';
 import { EditorViewComponent } from './editor-view.component';
 
 @NgModule({
-    imports: [SharedModule, TranslateModule],
+    imports: [SharedModule, TranslateModule, Ng2BasicModule],
     declarations: [
         EditorComponent, EditorTitleComponent, EditorFieldComponent,
         AppDataTableComponent, AppTableFieldComponent, EditorViewComponent
---
+++ src/lib/ng2/ng2.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { Ng2UiModule } from './ui/ui.module';
+
+@NgModule({
+    exports: [Ng2UiModule]
+})
+export class Ng2BasicModule { }
