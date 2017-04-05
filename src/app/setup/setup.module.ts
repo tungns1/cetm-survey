2c45cc40cb390b5208e1f84b5f564855131da3d9
Bui Nam <Bui Nam>
2017-04-05 17:30:14 +0700

    setup

--- src/app/setup/setup.module.ts
+++ src/app/setup/setup.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SetupComponent } from './setup/setup.component';
+import { ReactiveFormsModule ,FormsModule} from '@angular/forms';
+
+
+const routing = RouterModule.forChild([
+  { path: '', component: SetupComponent }
+]);
 
 @NgModule({
-  imports: [
+  imports: [routing,ReactiveFormsModule ,FormsModule,
     CommonModule
   ],
   declarations: [SetupComponent]
