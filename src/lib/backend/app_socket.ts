f5f3eba8594f88ec3eb8a8a4f13cbcb3e2f682e5
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-04-08 15:37:06 +0700

    new websocket

--- src/lib/backend/app_socket.ts
+++ src/lib/backend/app_socket.ts
@@ -1,46 +1,82 @@
 import { Socket } from './socket';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
-
-class SocketError {
-    constructor(protected type: string, protected message?: string) { }
-}
-
-const ConnectionError = new SocketError("Connection");
-
-class BadRequestError extends SocketError {
-    constructor(message: string) {
-        super("Bad Request", message);
-    }
-}
+import { Observable } from 'rxjs/Observable';
 
 import { LogService } from '../platform';
 
 import 'rxjs/add/operator/skip';
+import { BaseWebsocket, IBaseMessage, AbstractMessageHandler } from './web_socket';
 
-export class AppSocket extends Socket {
+class PrefixMessageHandler extends AbstractMessageHandler<IBaseMessage> {
+    deserialize(payload: string) {
+        payload = payload || '';
+        const ERROR_PREFIX = "/error";
+        const isError = payload.startsWith("/error");
+        if (isError) {
+            payload = payload.substring(ERROR_PREFIX.length);
+        }
+        const index = payload.indexOf(" ");
+        const uri = payload.substr(0, index);
+        const data = JSON.parse(payload.substring(index + 1));
+        const status = isError ? 'error' : 'success';
+        const res: IBaseMessage = { uri, data, status };
+        return res;
+    }
+
+    serialize(message: IBaseMessage) {
+        return `${message.uri} ${JSON.stringify(message.data)}`;
+    }
+}
+
+export class AppSocket extends BaseWebsocket {
     constructor(private uri: string, debug: boolean, logService: LogService) {
-        super(logService.Tag("Socket", debug));
+        super(new PrefixMessageHandler());
         this.init();
     }
 
     private init() {
-        this.Disconnected.skip(1).subscribe(() => {
-            this.Error$.next(ConnectionError);
-        });
-
-        this.Subscribe("/error", e => this.Error$.next(new BadRequestError(e['err'])));
-        this.Subscribe("/reload", () => setTimeout(() => {
+        this.reload$.subscribe(() => setTimeout(() => {
             window.location.reload();
         }, Math.random() * 1000));
     }
 
-    Connect<T>(params: T) {
+    Connect<P>(params: P) {
         const q = Object.keys(params).map(key => `${key}=${params[key]}`).join("&");
         const link = `${this.uri}?${q}`;
-        this._doConnect(link);
+        super.connect(link);
     }
 
-    Connected$ = this.rxConnected.filter(c => c);
-    Disconnected = this.rxConnected.filter(c => !c);
-    Error$ = new ReplaySubject<SocketError>(1);
+    Connected$ = this.Status$.filter(_ => this.isOpen);
+    Disconnected = this.Status$.filter(_ => !this.isOpen);
+
+    private reload$ = this.filter("/reload");
+
+    disableCheckAlive() {
+
+    }
+
+    Send<T>(uri: string, data: any): Observable<T> {
+        uri += `?once=${this.makeOnce()}`;
+        super.send(uri, data);
+        return this.filter(uri).first();
+    }
+
+    Subscribe<T>(uri: string, onEvent: (v: T) => void) {
+        return this.filter(uri).subscribe(onEvent);
+    }
+
+    Terminate() {
+        super.close();
+    }
+
+    RxEvent<T>(uri: string, replay = 1) {
+        const res = new ReplaySubject<T>(replay);
+        this.filter(uri).subscribe(res);
+        return res;
+    }
+
+    private makeOnce() {
+        return Math.random().toString(36).substring(7);
+    }
+
 }
