f0da83477c49b47dfecc54d5f208668e88fd7002
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-04-09 14:27:05 +0700

    add setting

--- src/app/counter/counter.module.ts
+++ src/app/counter/counter.module.ts
@@ -3,12 +3,13 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 import { SharedModule } from "./shared/";
 import { CounterComponent } from "./counter/counter.component";
 import { routing } from "./counter.routing";
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
     imports: [
         SharedModule, routing
     ],
-    declarations: [CounterComponent]
+    declarations: [CounterComponent, SettingComponent]
 })
 export class CounterModule {
 
---
+++ src/native/qms/qms.ts
@@ -0,0 +1,98 @@
+import {
+    Injectable, NgModule, ModuleWithProviders,
+    ValueProvider, FactoryProvider
+} from '@angular/core';
+import { Const } from '../../const';
+import {
+    AbstractStorageStrategy, LocalStorageStrategy
+} from '../../lib/platform';
+
+
+@Injectable()
+export class QmsService {
+    device(path: string): any {
+        console.log('device is not supported');
+        return null;
+    }
+    
+    __x: XWinService;
+
+    listen(event: string, cb: (event: string, args: any) => any) {
+
+    }
+
+    readonly isBrowser = true;
+}
+
+@Injectable()
+export class XWinService {
+    readonly isBrowser = true;
+}
+
+@Injectable()
+export class XWinStorageStrategy extends AbstractStorageStrategy {
+    constructor(
+        private qms: QmsService
+    ) {
+        super();
+        console.log('xwin storage');
+    }
+
+    private get xwin(): any {
+        return this.qms['__x'];
+    }
+
+    saveRaw(key: string, value: string) {
+        this.xwin.SetRuntime(key, value);
+    }
+
+    readRaw(key: string) {
+        return this.xwin.GetRuntime(key);
+    }
+}
+
+export function __newQmsService() {
+    return window['__QMS'] || new QmsService();
+}
+
+export function __newXWinService(qms: QmsService) {
+    return qms.isBrowser ? new XWinService() : qms['__x'];
+}
+
+export function __newXWinStorageStrategy(qms: QmsService) {
+    return qms.isBrowser ? new LocalStorageStrategy() :
+        new XWinStorageStrategy(qms);
+}
+
+export const QmsServiceProvider: FactoryProvider = {
+    provide: QmsService,
+    useFactory: __newQmsService
+}
+
+export const XWinServiceProvider: FactoryProvider = {
+    provide: XWinService,
+    deps: [QmsService],
+    useFactory: __newXWinService
+}
+
+export const XWinStorageProvider: FactoryProvider = {
+    provide: XWinStorageStrategy,
+    deps: [QmsService],
+    useFactory: __newXWinStorageStrategy
+}
+
+@NgModule({
+
+})
+export class QmsNativeModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: QmsNativeModule,
+            providers: [
+                QmsServiceProvider,
+                XWinServiceProvider,
+                XWinStorageProvider
+            ]
+        }
+    }
+}
