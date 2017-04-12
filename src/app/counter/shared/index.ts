08218ba83c1143d8a71d40e31b19fda6ce1889d0
Bui Nam <Bui Nam>
2017-04-12 09:32:30 +0700

    fix

--- src/app/counter/shared/index.ts
+++ src/app/counter/shared/index.ts
@@ -1,3 +1,6 @@
+import { CounterSettingService } from './counter-setting.service';
+import { CounterWelcomeGuard } from './counter-welcome.guard';
+import { AuthService } from '../../auth';
 export * from '../../shared/';
 export { CounterSettingService } from './counter-setting.service';
 export { CounterWelcomeGuard } from './counter-welcome.guard';
