53cb183cf3c7b0607ae7d7a2158b3931a2b29017
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-04-13 16:11:54 +0700

    add mouse track

--- src/app/counter/workspace/shared/device/index.ts
+++ src/app/counter/workspace/shared/device/index.ts
@@ -2,13 +2,11 @@ export * from './feedback.device';
 export * from './recorder.device';
 export * from './led.device';
 
-import { CounterDevice } from './counter.device';
 import { FeedbackDevice } from './feedback.device';
 import { LedDevice } from './led.device';
 import { RecorderDevice } from './recorder.device';
 
 export const workspaceDeviceProvider = [
-    CounterDevice,
     FeedbackDevice,
     LedDevice,
     RecorderDevice
