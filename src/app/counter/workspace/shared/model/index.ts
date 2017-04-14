c9968968829dba0d763fb8b1c72eb32857854ddf
Bui Nam <you@example.com>
2017-04-14 14:54:55 +0700

    fix

--- src/app/counter/workspace/shared/model/index.ts
+++ src/app/counter/workspace/shared/model/index.ts
@@ -9,13 +9,33 @@ export interface ITickets {
 }
 
 export function SortTicket(a: ITicket, b: ITicket) {
-    if (a.priority > b.priority) {
+
+    if (getPriority(a) > getPriority(b)) {
         return -1;
-    } else if (a.priority < b.priority) {
+    } else if (getPriority(a) < getPriority(b)) {
         return 1;
     }
     return a.mtime < b.mtime ? -1 : 1;
 }
+function getPriority(t: ITicket) {
+    console.log(t)
+    var priority = 0;
+    if (t.ticket_priority != undefined) {
+        if (t.ticket_priority.customer_vip != "") {
+            priority += parseInt(t.ticket_priority.customer_vip);
+        } else if (t.ticket_priority.service_priority != "") {
+            priority += parseInt(t.ticket_priority.service_priority);
+        } else if (t.ticket_priority.ticket_online != "") {
+            priority += parseInt(t.ticket_priority.ticket_online);
+        } else if (t.ticket_priority.vip_card != "") {
+            priority += parseInt(t.ticket_priority.vip_card);
+        } else if (t.ticket_priority.ticket_serving_move != "") {
+            priority += parseInt(t.ticket_priority.ticket_serving_move);
+        }
+        return priority;
+    }
+}
+
 
 export interface ITicketQueue {
     waiting: ITickets,
