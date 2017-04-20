374206fed44ff164f5084718413ceacfbe3ff78a
Bui Nam <you@example.com>
2017-04-20 17:40:17 +0700

    fix

--- src/app/shared/component/nav/top-nav.component.ts
+++ src/app/shared/component/nav/top-nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RuntimeEnvironment } from '../../env';
 
 @Component({
     selector: 'top-nav',
@@ -8,25 +9,28 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class TopNavComponent {
     constructor(
-        private router: Router
-    ) {}
-    @Input() role:string;
+        private router: Router,
+        private env: RuntimeEnvironment,
+    ) { }
+    role = '';
 
     ngOnInit() {
-        console.log(this.role);
+        this.env.Auth.User$.subscribe(u => {
+            this.role = u.role;
+        })
     }
 
     private isActive(route: string) {
-        if(this.router.url.indexOf('ticketlayout') > -1 && route.indexOf('kiosk') > -1) return true;
-        if(this.router.url.indexOf(route) > -1) {
-            if(this.router.url.startsWith('/admin')) {
-                if(this.router.url.indexOf('kiosk') > -1 && route.indexOf('kiosk') <= -1) return false;
-                if(this.router.url.indexOf('screen') > - 1 && route.indexOf('screen') <= -1) return false;
-                if(this.router.url.indexOf('ticketlayout') > -1 && route.indexOf('admin') > -1) return false; 
-                return true;                    
+        if (this.router.url.indexOf('ticketlayout') > -1 && route.indexOf('kiosk') > -1) return true;
+        if (this.router.url.indexOf(route) > -1) {
+            if (this.router.url.startsWith('/admin')) {
+                if (this.router.url.indexOf('kiosk') > -1 && route.indexOf('kiosk') <= -1) return false;
+                if (this.router.url.indexOf('screen') > - 1 && route.indexOf('screen') <= -1) return false;
+                if (this.router.url.indexOf('ticketlayout') > -1 && route.indexOf('admin') > -1) return false;
+                return true;
             }
             else return true;
-        } 
+        }
         else return false;
     }
 }
