0f32aff46e7ae73fadb9d2f193e6ffd7b23f053c
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-05-03 09:59:40 +0700

    [monitor] remove unused pipe

--- src/app/shared/pipe/index.ts
+++ src/app/shared/pipe/index.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { TimeModule } from '../../x/ng/time/';
 import { ServiceNamePipe, MultipleServiceNamePipe, TicketServiceNamePipe } from './serviceName';
-import { TicketStatePipe,TicketStateFinishPipe } from './ticketState';
+import { TicketStatePipe, TicketStateFinishPipe } from './ticketState';
 import { CounterNamePipe } from './counterName';
 import { UserFullNamePipe } from './userName';
-import { UppercaseFirstState } from './uppercaseState';
-import { BranchLevelNamePipe, BranchNamePipe,BranchNameKioskPipe } from './branch.pipe';
-import { TimeToHourPipe ,TimeToHourDeviceTrackPipe,TimePipe} from './time.pipe';
-import { TimeToDate } from './timeToDate.pipe';
+import { BranchLevelNamePipe, BranchNamePipe, BranchNameKioskPipe } from './branch.pipe';
 
 const exportComponents = [
-    ServiceNamePipe, MultipleServiceNamePipe, TicketServiceNamePipe,TicketStateFinishPipe,TimePipe,
-    TicketStatePipe, CounterNamePipe, UserFullNamePipe,BranchNameKioskPipe,
-    BranchLevelNamePipe, BranchNamePipe,UppercaseFirstState,TimeToHourPipe,TimeToDate,TimeToHourDeviceTrackPipe
+    ServiceNamePipe, MultipleServiceNamePipe, TicketServiceNamePipe, TicketStateFinishPipe,
+    TicketStatePipe, CounterNamePipe, UserFullNamePipe, BranchNameKioskPipe,
+    BranchLevelNamePipe, BranchNamePipe
 ]
 
 @NgModule({
