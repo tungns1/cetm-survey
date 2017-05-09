26c80aa4ebff16a0d017ccd3c22375eb101f3c06
tanvd <tanvd@miraway.vn>
2017-05-09 09:29:24 +0700

    change some UI

--- src/app/report/component/history/history.component.ts
+++ src/app/report/component/history/history.component.ts
@@ -51,6 +51,7 @@ export class HistoryComponent {
     showDetails(tr: ITransactionView) {
         const config = new MdDialogConfig();
         config.width = '350px';
+        config.height = '425px';
         const dialog = this.mdDialog.open(TransactionComponent, config); 
         dialog.componentInstance.SetData(tr);
     }
