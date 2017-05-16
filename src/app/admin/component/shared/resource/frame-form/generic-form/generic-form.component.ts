8248b2633a585e80469038aac1d50dffb22e7ca2
tanvd <tanvd@miraway.vn>
2017-05-16 09:13:13 +0700

    finished the shit

--- src/app/admin/component/shared/resource/frame-form/generic-form/generic-form.component.ts
+++ src/app/admin/component/shared/resource/frame-form/generic-form/generic-form.component.ts
@@ -42,7 +42,8 @@ export class GenericFormComponent {
     if (!this.record) {
       return;
     }
-    console.log(this.record);
+    // console.log(this.record);
+    // this.record.data.repeat = [{"hour":"10:00 AM - 5:00 PM","store":"LUNES - VIERNES"},{"hour":"09:30 AM - 4:30 PM","store":"SÁBADO - DOMINGO"}];
     this.dialog.close(this.record);
   }
 
