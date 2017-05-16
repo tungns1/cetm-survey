6424d1ae85359269eb6be169d784bf842672c84c
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-05-16 16:44:49 +0700

    [ng] file picker component

--- src/app/x/ng/upload/file-picker/file-picker-modal.component.ts
+++ src/app/x/ng/upload/file-picker/file-picker-modal.component.ts
@@ -10,32 +10,22 @@ export class FilePickerModalComponent implements OnInit {
 
     constructor(
         @Optional() @Inject(MD_DIALOG_DATA) private dialogData: any,
-        private dialog: MdDialog,
+        private dialog: MdDialogRef<FilePickerModalComponent>,
     ) { }
 
     value: string;
-    change;
-    protected onChangeCallback = (v) => { };
-
-    @HostListener("change") onChange() {
-        this.onChangeCallback(this.value);
-    }
 
     ngOnInit() {
-        // console.log('aaaaaaaaaaaaa');
-        // console.log(this.dialogData);
         this.value = this.dialogData.value;
-        this.change = this.dialogData.change;
     }
 
     close() {
-        this.dialog.closeAll();
+        this.dialog.close(null);
     }
 
     choose(node: FileNode) {
         this.value = node.Path;
-        this.onChange();
-        this.change.next();
+        this.dialog.close(this.value);
     }
 
 }
