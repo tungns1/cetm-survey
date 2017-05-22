e1e69b056d62dcee1ee892aa5385472b10779a6d
damminh <minhdv02011993@gmail.com>
2017-05-22 17:41:32 +0700

    validate

--- src/app/admin/component/house/counter/counter.component.ts
+++ src/app/admin/component/house/counter/counter.component.ts
@@ -27,11 +27,12 @@ export class CounterComponent extends BaseAdminComponent<ICounter> {
     }
 
     services = this.org.ServiceService.RxListView;
+    pattern_code: any ="^[a-zA-Z][a-zA-Z0-9-_]{5,19}$";
     makeForm(b?: ICounter) {
         b = b || <any>{};
         return (new FormBuilder).group({
             id: [b.id],
-            code: [b.code, Validators.required],
+            code: [b.code, Validators.compose([Validators.required, Validators.pattern(this.pattern_code)])],
             name: [b.name, Validators.required],
             cnum: [b.cnum, Validators.required],
             dev_addr: [b.dev_addr],
