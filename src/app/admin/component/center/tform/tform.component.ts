3f2581757d444db43030e3f34d5cc00ed3e0a7ef
damminh <minhdv02011993@gmail.com>
2017-05-22 21:12:58 +0700

    validate form

--- src/app/admin/component/center/tform/tform.component.ts
+++ src/app/admin/component/center/tform/tform.component.ts
@@ -20,12 +20,14 @@ export class TFormComponent extends BaseAdminComponent<ITForm> {
 
   title = 'Ticket Number Format'
 
+  pattern_format: any ="^[A-Z][%]0?([1-9])[d][A-Z]$";
+  pattern_code: any ="^[a-zA-Z][a-zA-Z0-9-_]{5,19}$";
   makeForm(b?: ITForm) {
     b = b || <any>{};
     return (new FormBuilder).group({
       id: [b.id],
-      code: [b.code, Validators.required],
-      format: [b.format, Validators.required],
+      code: [b.code, Validators.compose([Validators.required, Validators.pattern(this.pattern_code)])],
+      format: [b.format, Validators.compose([Validators.required, Validators.pattern(this.pattern_format)])],
       minnum: [b.minnum || 0, Validators.required],
       maxnum: [b.maxnum || 999, Validators.required],
       template: [b.template]
