d1f57112383774035b11242878d841ea8725b1ff
damminh <minhdv02011993@gmail.com>
2017-05-22 15:15:33 +0700

    validate user

--- src/app/admin/component/org/user/user.component.ts
+++ src/app/admin/component/org/user/user.component.ts
@@ -19,19 +19,21 @@ export class UserComponent extends BaseAdminComponent<IUser> {
   }
 
   title = 'user';
-
+  pattern_user: any ="^[a-zA-Z][a-zA-Z0-9-_]{5,19}$";
+  pattern_pass: any ="^[a-zA-Z][a-zA-Z0-9-_\?\!\@\#\$\*]{5,19}$";
   makeForm(u?: IUser) {
     u = u || <any>{};
     return (new FormBuilder).group({
       id: [u.id],
-      password: ['', u.id ? null : Validators.required], // do not require password on update
-      username: [u.username, Validators.required],
+      password: ['', u.id ? null : Validators.compose([ Validators.required, Validators.pattern(this.pattern_pass)])], // do not require password on update
+      username: [ u.username, Validators.compose([ Validators.required, Validators.pattern(this.pattern_user)])],
       fullname: [u.fullname, Validators.required],
       email: [u.email],
       role: [u.role, Validators.required],
       branch_id: [u.branch_id, Validators.required],
     });
   }
+
 
   private roles = AllRoles;
   private branches = this.org.BranchService.RxListView;
