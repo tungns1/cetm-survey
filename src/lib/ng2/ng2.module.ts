9fc0fe531dc6f654f4f56b0a522aafb3c4d3a63a
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-05-24 15:29:49 +0700

    [report] export data to excel

--- src/lib/ng2/ng2.module.ts
+++ src/lib/ng2/ng2.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PipeModule } from './pipe/pipe.module';
 import { Ng2UiModule } from './ui/ui.module';
+import { Ng2DataModule } from './data/data.module';
 
 @NgModule({
-    exports: [PipeModule, Ng2UiModule]
+    exports: [PipeModule, Ng2UiModule, Ng2DataModule]
 })
 export class Ng2BasicModule { }
