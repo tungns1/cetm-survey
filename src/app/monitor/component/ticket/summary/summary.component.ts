ce8c4f2ad30d7d01822cb0206992d3e7a11914e1
tanvd <tanvd@miraway.vn>
2017-06-08 17:41:48 +0700

    working on ag-grid in monitor - summary; almost done

--- src/app/monitor/component/ticket/summary/summary.component.ts
+++ src/app/monitor/component/ticket/summary/summary.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import {
     ISummary, Summary,
-    MonitorNavService, MonitorFilterService
+    MonitorNavService, MonitorFilterService, CacheBranch
 } from '../../shared';
 import { MonitorSummaryService, ProjectConfig } from '../shared';
+import { GridOptions } from "ag-grid";
 
 @Component({
     selector: 'ticket-summary',
@@ -19,22 +20,74 @@ export class SummaryComponent {
         private navService: MonitorNavService,
         private filterService: MonitorFilterService,
         private summaryService: MonitorSummaryService,
-      
+
     ) { }
 
     records$ = this.summaryService.summaries$.map(s => s.ToArray()).share();
     total$ = this.summaryService.summaries$.switchMap(s => s.Total$);
-    waitLongAlertPercent: number;
-    serveLongAlertPercent: number;
+
+    private gridOptions: GridOptions = {
+        rowHeight: 35,
+        floatingBottomRowData: [],
+        getRowStyle: (e) => {
+
+        },
+        onCellClicked: (e) => {
+            if (e.data.branch_id) {
+                this.router.navigate(['../focus', e.data.branch_id], {
+                    relativeTo: this.route,
+                    queryParamsHandling: "merge"
+                });
+            }
+        }
+    };
 
     ngOnInit() {
-        this.waitLongAlertPercent = ProjectConfig.service.wait_long_alert_percent;
-        this.serveLongAlertPercent = ProjectConfig.service.serve_long_alert_percent;
         this.navService.Refresh$.ExclusiveSubscribe(_ => {
             this.summaryService.Branches$.next(
                 this.filterService.GetStores()
             );
         });
+        this.total$.subscribe(t => {
+            this.gridOptions.api.setFloatingBottomRowData([t]);
+            this.gridOptions.api.getFloatingBottomRow(0).canFlower;
+        });
+    }
+
+    storeCellRenderer(d) {
+        if (d.data.branch_id)
+            return CacheBranch.GetNameForID(d.data.branch_id);
+        else return 'Summary';
+    }
+
+    exceededWaitingCellRenderer(d) {
+        return '<span>' + d.data.wait_long + ' (' + d.data.w_l_percent + '%)</span>';
+    }
+
+    warningExceededWaiting(d) {
+        if (d.data.branch_id) {
+            if (d.data.w_l_percent > ProjectConfig.service.wait_long_alert_percent) {
+                return {
+                    backgroundColor: '#ff5858',
+                    color: '#fff'
+                }
+            }
+        }
+    }
+    /////////////////////////////////////
+    exceededServingCellRenderer(d) {
+        return '<span>' + d.data.serve_long + ' (' + d.data.s_l_percent + '%)</span>';
+    }
+
+    warningExceededServing(d) {
+        if (d.data.branch_id) {
+            if (d.data.s_l_percent > ProjectConfig.service.serve_long_alert_percent) {
+                return {
+                    backgroundColor: '#ff5858',
+                    color: '#fff'
+                }
+            }
+        }
     }
 
     focus(s: ISummary) {
