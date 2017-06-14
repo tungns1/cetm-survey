9944f8139ca6a04ab48d96a6643505bf77275e02
Bui Nam <you@example.com>
2017-06-14 14:55:03 +0700

    fix counter setting branch_code and counter_code

--- src/app/counter/setting/setting.component.ts
+++ src/app/counter/setting/setting.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { CounterSettingService } from '../shared';
+import { CounterSettingService, AuthService, RuntimeEnvironment } from '../shared';
+import { Router } from '@angular/router'
+import { CacheBranch } from '../../shared/model';
+import { AppStorage } from '../../shared';
 
 @Component({
   selector: 'app-setting',
@@ -10,7 +13,10 @@ import { CounterSettingService } from '../shared';
 export class SettingComponent implements OnInit {
 
   constructor(
-    private counterSetting: CounterSettingService
+    private counterSetting: CounterSettingService,
+    private router: Router,
+    private authService: AuthService,
+    private evn: RuntimeEnvironment,
   ) { }
 
   ngOnInit() {
@@ -26,10 +32,18 @@ export class SettingComponent implements OnInit {
     branch_code: new FormControl(this.value.branch_code),
     counter_code: new FormControl(this.value.counter_code),
     mini_mode: new FormControl(this.value.mini_mode),
-    
+
     enable_recording: new FormControl(this.value.enable_recording),
     led_addr: new FormControl(this.value.led_addr),
     led_com_port: new FormControl(this.value.led_com_port)
   });
+
+  Go() {
+    const newValue = this.form.value;
+    if (newValue.branch_code !== this.value.branch_code || newValue.counter_code !== this.value.counter_code) {
+      AppStorage.ClearToken();
+    }
+    this.router.navigate(['../counter/workspace'])
+  }
 
 }
