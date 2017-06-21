4262433c6b8cc2b04cc5fc3e9350064e00ab946f
Bui Nam <you@example.com>
2017-06-21 11:26:45 +0700

    fix

--- src/app/counter/workspace/shared/service/feedback.service.ts
+++ src/app/counter/workspace/shared/service/feedback.service.ts
@@ -47,6 +47,7 @@ export class FeedbackService {
     // return [] if can next
     // return null if not
     CheckFeedback(t: Ticket) {
+        console.log(t)
         if (t) {
             var feedback = t.tracks[t.tracks.length - 1].feedback || null;
             if (feedback) {
