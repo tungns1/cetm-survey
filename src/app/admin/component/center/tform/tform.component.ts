0b45ed0a1b6eafe517101e9b05ac0999c7decf43
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-06-25 12:58:30 +0700

    [admin] allow number in ticket format

--- src/app/admin/component/center/tform/tform.component.ts
+++ src/app/admin/component/center/tform/tform.component.ts
@@ -25,14 +25,14 @@ export class TFormComponent extends BaseAdminComponent<ITForm> {
   }
 
   title: string = 'Ticket number format';
-  pattern_format: any = "^[A-Z]?[%]0?([1-9])[d][A-Z]?$";
+  ticket_format_pattern = "^[0-9a-zA-Z]?[%]0?([1-9])[d][A-Z]?$";
 
   makeForm(b?: ITForm) {
     b = b || <any>{};
     return (new FormBuilder).group({
       id: [b.id],
       code: [b.code, CommonValidator.Code],
-      format: [b.format, Validators.compose([Validators.required, Validators.pattern(this.pattern_format)])],
+      format: [b.format, Validators.compose([Validators.required, Validators.pattern(this.ticket_format_pattern)])],
       minnum: [b.minnum || 0, Validators.required],
       maxnum: [b.maxnum || 999, Validators.required],
       template: [b.template]
