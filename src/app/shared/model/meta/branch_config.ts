b5513441576c2dc2d76a120de2d1f7728df2be4e
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-06-25 14:03:43 +0700

    [const] update priority from bconf

--- src/app/shared/model/meta/branch_config.ts
+++ src/app/shared/model/meta/branch_config.ts
@@ -1,3 +1,5 @@
+import { IPriorityConfig } from '../../../../const/project/priority';
+
 interface IServiceConfig {
     basket: string[];
     max_waiting_minute: number;
@@ -10,47 +12,10 @@ interface IFeedbackConfig {
     remind_timeout: number;
 }
 
-interface IKioskConfig {
-
-}
-
-interface ILedConfig {
-    show_error_on_counter: boolean;
-}
-
-interface IMonitorConfig {
-
-}
-
-interface IReportConfig {
-
-}
-
-interface IScreenConfig {
-
-}
-
-interface ISystemConfig {
-
-}
-
-interface ICounterConfig {
-    max_concurrent_serving: number;
-    min_call_before_miss: number;
-    auto_finish_wait_minute: number;
-    auto_finish_retry_minute: number;
-}
-
-interface IL10nConfig {
-    locale: string;
-    locales: string[];
-}
-
 export interface IBranchConfig {
     id: string;
     branch_id: string;
     service: IServiceConfig;
     feedback: IFeedbackConfig;
-    counter: ICounterConfig;
-    l10n: IL10nConfig;
+    priority: IPriorityConfig;
 }
