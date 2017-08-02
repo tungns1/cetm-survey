00638de3aa9a1ddc2c1d5aa2c6ec450eba03af81
namhd <you@example.com>
2017-08-02 02:05:02 -0500

    fix

--- src/app/report/shared/model/kiosk_effective.ts
+++ src/app/report/shared/model/kiosk_effective.ts
@@ -86,18 +86,43 @@ export class InfoKioskTrack {
     Add(s: IKioskEffective) {
 
         if (s != null) {
-            if (s.time.length > 0 && s.ticket.length > 0) {
-                this.total_activity_time = this.SecondToHour(sumBy(s.time, 'a_d'))
-                s.ticket.forEach(i => {
-                    this.total_ticket += i.count;
-                })
-                this.total_kiosk = size(groupBy(s.time, 'eid'));
+            if(s.time.length > 0 ){
+                 this.total_activity_time = this.SecondToHour(sumBy(s.time, 'a_d'))
+                  this.total_kiosk = size(groupBy(s.time, 'eid'));
                 var data_by_branh = toArray(groupBy(s.time, 'bid'));
 
 
                 var data_by_date = toArray(groupBy(s.time, 'date'));
-                var len_by_date = size(data_by_date);
+                 var len_by_date = size(data_by_date);
                 var len_by_branch = size(data_by_branh);
+                 for (var i = 0; i < len_by_branch; i++) {
+                    this.time.push({
+                        name: CacheBranch.GetNameForID(data_by_branh[i][0].bid),
+                        value: this.SecondToHour(sumBy(data_by_branh[i], 'a_d'))
+                    })
+
+                    this.time_sum.push({
+                        name: CacheBranch.GetNameForID(data_by_branh[i][0].bid),
+                        total: this.SecondToHour(sumBy(data_by_branh[i], 'a_d') || 0),
+                        longest: this.SecondToHour(maxBy(data_by_branh[i], 'a_d').a_d),
+                        shortest: this.SecondToHour(minBy(data_by_branh[i], 'a_d').a_d),
+                        average: this.SecondToHour(meanBy(data_by_branh[i], <any>'a_d')),
+                    })
+                }
+                  for (var i = 0; i < len_by_date; i++) {
+                    this.time_day.push({
+                        name: data_by_date[i][0].date,
+                        value: this.SecondToHour(sumBy(data_by_date[i], 'a_d'))
+                    })
+                }
+            }
+            if (s.ticket.length > 0) {
+              
+                s.ticket.forEach(i => {
+                    this.total_ticket += i.count;
+                })
+               
+               
 
                 var data_by_branh_t = toArray(groupBy(s.ticket, 'branch_id'));
 
@@ -138,20 +163,7 @@ export class InfoKioskTrack {
                         average: +(total / len_by_date_t).toFixed(2),
                     })
                 }
-                for (var i = 0; i < len_by_branch; i++) {
-                    this.time.push({
-                        name: CacheBranch.GetNameForID(data_by_branh[i][0].bid),
-                        value: this.SecondToHour(sumBy(data_by_branh[i], 'a_d'))
-                    })
-
-                    this.time_sum.push({
-                        name: CacheBranch.GetNameForID(data_by_branh[i][0].bid),
-                        total: this.SecondToHour(sumBy(data_by_branh[i], 'a_d') || 0),
-                        longest: this.SecondToHour(maxBy(data_by_branh[i], 'a_d').a_d),
-                        shortest: this.SecondToHour(minBy(data_by_branh[i], 'a_d').a_d),
-                        average: this.SecondToHour(meanBy(data_by_branh[i], <any>'a_d')),
-                    })
-                }
+               
                 for (var i = 0; i < len_by_date_t; i++) {
                     var t = 0;
                     data_by_date_t[i].forEach(i => {
@@ -162,12 +174,7 @@ export class InfoKioskTrack {
                         value: t,
                     })
                 }
-                for (var i = 0; i < len_by_date; i++) {
-                    this.time_day.push({
-                        name: data_by_date[i][0].date,
-                        value: this.SecondToHour(sumBy(data_by_date[i], 'a_d'))
-                    })
-                }
+              
             }
         }
 
@@ -186,13 +193,18 @@ export class InfoKioskTrack {
 
 
         if (s != null) {
-            if (s.time.length > 0 && s.ticket.length > 0) {
+            if(s.time.length > 0){
                 this.longest_activity_time = maxBy(this.time, 'value').value;
                 this.longest_activity_kiosk = maxBy(this.time, 'value').name;
                 this.shortest_activity_time = minBy(this.time, 'value').value;
                 this.shortest_activity_kiosk = minBy(this.time, 'value').name;
                 this.average_activity_time = +meanBy(this.time, <any>'value').toFixed(2);
                 this.average_kiosk_eff = +(this.total_activity_time / this.total_kiosk).toFixed(2);
+                  this.time_day.sort(function (a, b) { return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); });
+                this.time = this.time.sort((a, b) => b.value - a.value).slice(0, 5);
+            }
+            if (s.ticket.length > 0) {
+               
                 this.highest_ticket_quantity = maxBy(this.ticket, 'value').value;
                 this.highest_ticket_from = maxBy(this.ticket, 'value').name;
                 this.lowest_ticket_quantity = minBy(this.ticket, 'value').value;
@@ -200,8 +212,7 @@ export class InfoKioskTrack {
                 this.average_printed_ticket = +meanBy(this.ticket, <any>'value').toFixed(2);
                 this.ticket = this.ticket.sort((a, b) => b.value - a.value).slice(0, 5);
                 this.ticket_day.sort(function (a, b) { return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); });
-                this.time_day.sort(function (a, b) { return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); });
-                this.time = this.time.sort((a, b) => b.value - a.value).slice(0, 5);
+              
             }
         }
 
