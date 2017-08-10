7402c3a05e6010abbfe1d4db63a07bd44a67071c
tanvd <tanvd@miraway.vn>
2017-08-10 14:09:37 +0700

    add slider-form to show layout resource of screen

---
+++ src/app/admin/component/shared/resource/frame-form/slider-form/slider-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SliderFormComponent } from './slider-form.component';
+
+describe('SliderFormComponent', () => {
+  let component: SliderFormComponent;
+  let fixture: ComponentFixture<SliderFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SliderFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SliderFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
