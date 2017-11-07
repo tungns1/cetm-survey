2c3e9c62f57b8b6dd9f248818deeeba3b3b06ebb
tanvd <tanvd@miraway.vn>
2017-11-07 16:52:40 +0700

    fix css

--- src/app/counter/workspace/workspace/normal/normal.component.ts
+++ src/app/counter/workspace/workspace/normal/normal.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { WorkspaceService } from '../../shared';
 
 @Component({
   selector: 'app-normal-workspace',
   templateUrl: './normal.component.html',
-  styleUrls: ['./normal.component.scss']
+  styleUrls: ['./normal.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NormalWorkspaceComponent implements OnInit {
 
