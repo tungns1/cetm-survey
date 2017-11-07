837a1f4e9aee686634d2d03a70e0c6d1a5469bdf
tanvd <tanvd@miraway.vn>
2017-11-07 09:10:01 +0700

    try to re rendering view

--- src/app/counter/super-counter/counter-detail/counter-detail.component.ts
+++ src/app/counter/super-counter/counter-detail/counter-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, NgZone, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {
   trigger, state, style,
   animate, transition
@@ -17,24 +17,30 @@ import { counterDetail } from '../shared/model';
 export class CounterDetailComponent implements OnInit {
 
   constructor(
-    private _zone: NgZone,
     private superCounterService: SuperCounterService,
   ) { }
 
-  @Input() counter: counterDetail;
+  _counter: counterDetail;
+  @Input() set counter(c: counterDetail) {
+    this._counter = c;
+    let header = document.getElementById('counterDetailHeader');
+    if (header) {
+      header.classList.remove('fadeIn');
+      setTimeout(_ => {
+        header.classList.add('fadeIn');
+      })
+    }
+
+  };
   maxServingMinute = ProjectConfig.service.max_serving_minute;
-  checked = this.counter && this.counter.state === 'serving';
+  checked = this._counter && this._counter.state === 'serving';
 
   ngOnInit() {
   }
 
-  ngAfterViewInit(){
-    this._zone.run(() => console.log('done'))
-  }
-
   checkIn() {
-    if (this.counter.ticketNum)
-      this.superCounterService.markAsCheck(this.counter.counterID, this.counter.serving);
+    if (this._counter.ticketNum)
+      this.superCounterService.markAsCheck(this._counter.counterID, this._counter.serving);
   }
 
 }
