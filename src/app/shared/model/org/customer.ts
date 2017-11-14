ed6688b34aea4d69f96db37cf92c91e5028ac646
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-11-14 10:27:02 +0700

    [model] customer name default to ''

--- src/app/shared/model/org/customer.ts
+++ src/app/shared/model/org/customer.ts
@@ -16,16 +16,16 @@ export class Customer {
 
     constructor(private v: ICustomer) { }
     id = this.v.id;
-    email = this.v.email;
-    code = this.v.code;
+    email = this.v.email || '';
+    code = this.v.code || '';
     date_of_birth = this.v.date_of_birth;
-    first_name = this.v.first_name;
-    last_name = this.v.last_name;
-    phone_number = this.v.phone_number;
-    nationality = this.v.nationality;
-    vip_code = this.v.vip_code;
-    segment = this.v.segment;
+    first_name = this.v.first_name || '';
+    last_name = this.v.last_name || '';
+    phone_number = this.v.phone_number || '';
+    nationality = this.v.nationality || '';
+    vip_code = this.v.vip_code || '';
+    segment = this.v.segment || '';
     full_name = this.v.full_name || this.first_name + ' ' + this.last_name;
-    short_name = this.v.short_name;
+    short_name = this.v.short_name || '';
 
 }
