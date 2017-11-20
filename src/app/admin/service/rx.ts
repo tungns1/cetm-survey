23b39a31bc95ac85356f4507f44a18a54bf70989
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2017-11-20 09:47:52 +0700

    [admin] fix rx path

--- src/app/admin/service/rx.ts
+++ src/app/admin/service/rx.ts
@@ -2,13 +2,13 @@
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
-import 'rxjs/add/operator/combinelatest';
+import { combineLatest } from 'rxjs/observable/combinelatest';
 import 'rxjs/add/operator/switchMap';
 
 export class RepeatedObservable<T> extends Observable<T> {
   constructor(source: Observable<any>, func: (a: any) => Observable<T>) {
     super((observer: Observer<T>) => {
-      Observable.combineLatest<any, any>(this.rxRefresh, source).switchMap(values => func(values[1])).subscribe(observer);
+      combineLatest<any, any>(this.rxRefresh, source).switchMap(values => func(values[1])).subscribe(observer);
     });
   }
   private rxRefresh = new BehaviorSubject<boolean>(true);
