b7f1ef53970f8d5cdcb02229dce5ea53262f96ee
tanvd <tanvd@miraway.vn>
2018-06-14 11:05:51 +0700

    del some func of booking

--- src/app/counter/workspace/shared/service/workspace.service.ts
+++ src/app/counter/workspace/shared/service/workspace.service.ts
@@ -13,48 +13,6 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 const SOCKET_LINK = "/room/counter/join";
 
-// export interface ITicketBooking {
-//     id: string;
-//     created_at: number;
-//     updated_at: number;
-//     customer: ICustomer;
-//     time_go_bank: number;
-//     service_id: string;
-//     branch_id: string;
-//     type_ticket: string;
-//     lang: string;
-//     customer_code: string;
-//     customer_id: string;
-//     check_in_at: number;
-//     id_ticket_cetm: string;
-//     cnum_cetm: string;
-//     teller_id: string;
-//     teller: string;
-//     serving_time: string;
-//     wating_time: string;
-//     status: string;
-// }
-
-interface IBookingOnlineRespone {
-    status: string;
-    code: number;
-    data: ITicketBooking[];
-}
-
-interface ISyncBookingTicket {
-    teller: string;
-    avatar_teller: string;
-    teller_id: string;
-    bticket_id?: string;
-    check_in_at: number;
-    id_ticket_cetm: string;
-    cnum_cetm: string;
-    status: string;
-    serving_time: number;
-    waiting_time: number;
-    serving_at?: number;
-}
-
 @Injectable()
 export class WorkspaceService {
     constructor(
@@ -87,9 +45,6 @@ export class WorkspaceService {
                     }));
                 } else {
                     w.Update(action);
-                    if (action.ticket.id) {
-                        this.syncBookingSystem(action);
-                    }
                 }
             });
         const autoNext = this.autoNext$.map(a => {
@@ -119,7 +74,6 @@ export class WorkspaceService {
         this.env.Auth.Data$.subscribe(data => this.currentBranch = data.branches.find(branch => branch.name === data.store));
         this.currentCounter$.subscribe(counter => {
             this.currentCounter = counter;
-            this.getBookingOnlineList();
         });
     }
 
@@ -129,64 +83,5 @@ export class WorkspaceService {
     SetAutoNext(auto = false) {
         this.autoNext$.next(auto);
     }
-
-    getBookingOnlineList() {
-        let params = new HttpParams().set('branch_id', this.currentBranch.id)
-        this.httpClient.get('http://123.31.12.147:8989/api/booking/ticket/branch_cetm_tickets', { params: params })
-            .subscribe((respone: IBookingOnlineRespone) => {
-                if (respone.status === 'ok' && respone.data)
-                    this.bookingOnlineList$.next(respone.data.filter(d => {
-                        return (d.type_ticket === 'book_schedule' && (this.currentCounter.services.indexOf(d.service_id) != -1));
-                    }).sort((a, b) => {
-                        if (a.check_in_at >= b.check_in_at) return 1;
-                        if (a.check_in_at < b.check_in_at) return -1;
-                    }));
-            });
-    }
-
-    private updateBooking(bookingTicket: ITicketBooking) {
-        this.bookingOnlineList$
-    }
-
-    private syncBookingSystem(action: ITicketAction) {
-        if ((action.action === 'finish' || action.action === 'move' || action.action === 'cancel') && action.counter_id === this.currentCounter.id) {
-            let body: ISyncBookingTicket = {
-                teller: this.currentUser.fullname,
-                avatar_teller: this.currentUser.public_avatar || '',
-                teller_id: this.currentUser.id,
-                bticket_id: action.ticket.ticket_booking.id,
-                check_in_at: action.ticket.ticket_booking.check_in_at,
-                id_ticket_cetm: action.ticket.id,
-                cnum_cetm: action.ticket.cnum,
-                status: action.ticket.state,
-                serving_time: this.getServingTime(JSON.parse(JSON.stringify(action.ticket.tracks)).reverse()),
-                waiting_time: this.getWaitingTime(JSON.parse(JSON.stringify(action.ticket.tracks))),
-                serving_at: this.getServingAt(this.getServingTime(JSON.parse(JSON.stringify(action.ticket.tracks)).reverse()))
-            }
-            this.httpClient.post('http://123.31.12.147:8989/api/booking/ticket/cetm_update', body).subscribe();
-        }
-    }
-
-    private getServingTime(tracksReverse: ITicketTrack[]): number {
-        let lastServingTime = tracksReverse.find(checkPoint => checkPoint.state === 'serving');
-        if (lastServingTime) {
-            return tracksReverse[0].mtime - lastServingTime.mtime;
-        }
-        return 0;
-    }
-
-    private getWaitingTime(tracks: ITicketTrack[]): number {
-        let result: number = 0;
-        tracks.forEach((track, i) => {
-            if (track.state === 'waiting') {
-                result += tracks[i + 1] ? (tracks[i + 1].mtime - track.mtime) : 0;
-            }
-        })
-        return result;
-    }
-
-    private getServingAt(serving_time: number):number{
-        let now = Math.floor(Date.now() / 1000)
-        return (now - serving_time)
-    }
+    
 }
