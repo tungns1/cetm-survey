21fcfab75a9e2cb548e3fd6580e2c2aa9b47bb43
tanvd <tanvd@miraway.vn>
2018-06-18 15:03:28 +0700

    fix bug import

--- src/app/counter/workspace/serving/serving.module.ts
+++ src/app/counter/workspace/serving/serving.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { TicketModule } from '../ticket';
 import { ServingComponent } from './serving.component';
 import { EmptyComponent } from './empty.component';
-import { ActionComponent } from './action.component';
 import { TicketComponent } from './ticket.component';
 import { ModalModule } from '../shared';
-import { CustomerInfoComponent } from './customer/customer-info.component';
 import { AddServiceDialogComponent } from './add-service-dialog/add-service-dialog.component';
 
 @NgModule({
     imports: [TicketModule],
     declarations: [
-        ServingComponent, EmptyComponent, ActionComponent,
-        TicketComponent, CustomerInfoComponent, AddServiceDialogComponent
+        ServingComponent, EmptyComponent,
+        TicketComponent, AddServiceDialogComponent
     ],
     exports: [ServingComponent],
     entryComponents: [AddServiceDialogComponent]
