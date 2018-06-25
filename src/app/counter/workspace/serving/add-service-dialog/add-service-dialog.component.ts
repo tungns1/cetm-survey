92deee43139ed286eed4d512f29d8b917a7eaee9
tung92.ns <tung92.ns@gmail.com>
2018-06-25 16:19:55 +0700

    update thêm dịch vụ counter chỉ thêm đc dịch vụ mà counter đó có

--- src/app/counter/workspace/serving/add-service-dialog/add-service-dialog.component.ts
+++ src/app/counter/workspace/serving/add-service-dialog/add-service-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { Optional } from 'ag-grid';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { Ticket, WorkspaceService, TicketService, NoticeComponent } from '../../shared';
+import { Ticket, WorkspaceService, TicketService, NoticeComponent, ServiceName } from '../../shared';
 import { ShowLoading, HideLoading } from '../../../../../lib/backend';
 
 @Component({
@@ -17,9 +17,20 @@ export class AddServiceDialogComponent  {
         private dialogRef: MatDialogRef<AddServiceDialogComponent>,
         private ticketService: TicketService,
     ) { }
-    services = this.workspaceService.services$;
+    services = [];
     checkedServices = [];
     @ViewChild(NoticeComponent) notice: NoticeComponent;
+
+    ngOnInit(){
+        this.workspaceService.currentCounter$.subscribe(v => {
+            this.services = v.services.map(item => {
+                return Object.assign({
+                    id: item,
+                    name: ServiceName(item)
+                })
+            })
+        })
+    }
 
     close(){
         this.dialogRef.close();
